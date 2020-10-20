3c1467dd541d51a4dbd4fe0461d6af7e28181e9e
Anastasija Javorska <alex.prostjakov@gmail.com>
2020-10-20 17:37:33 +0300

    Day 2 tasks

--- SEF/src/sef/module4/activity/CalculatorClient.java
+++ SEF/src/sef/module4/activity/CalculatorClient.java
@@ -14,7 +14,7 @@ public class CalculatorClient {
 		/*Instantiate Normal Calculator. Note that CALC_TYPE_NORMAL is a public static
 		 *final variable defined in Calculator class. 
 		*/
-		Calculator normalCalc = new Calculator(Calculator.CALC_TYPE_BASIC);
+		Calculator normalCalc = new Calculator(Calculator.CALC_TYPE_NORMAL);
 		
 		System.out.println("*** Calculations will be performed using "+normalCalc.getCalculatorType()+ " calculator ***");
 		
@@ -22,11 +22,19 @@ public class CalculatorClient {
 		int result = normalCalc.add(4, 5);
 		System.out.println("1) Add result is " + result);
 		
-				
+		//Subtract
+		int result1 = normalCalc.subtract(4, 5);
+		System.out.println("1) Subtract result is " + result1);
+		
+		
 		// Multiply 
 		int[] mul = {2, 3, 4};
 		result = normalCalc.multiply(mul);
-		System.out.println("2) Multiplication result is " + result); 
+		System.out.println("3) Multiplication result is " + result); 
+		
+		//Division
+		int result4 = normalCalc.divide(10, 5);
+		System.out.println("4) Division result is " + result4);		
 		
 
 	}
